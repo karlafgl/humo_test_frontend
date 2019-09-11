31a0d2a0ae0be6eab1aa709af353e3d39b4ba8d5
karla <karla@Threde-LLC.local>
2019-09-10 18:45:05 -0600

    adding scripts and styles

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log('working')
+
+
+document.addEventListener("DOMContentLoaded", $('.slider').slick({
+    vertical: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    speed: 300
+  }));
+
